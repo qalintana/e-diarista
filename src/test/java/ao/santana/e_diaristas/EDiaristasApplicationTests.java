d2fd102a4213e8ccb39f08c34d0c4fa4348b0cc3
Queiroz Santana <alintana18@gmail.com>
2024-09-11 20:43:32 +0100

    configurando database e adicionando templates

---
+++ src/test/java/ao/santana/e_diaristas/EDiaristasApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.santana.e_diaristas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDiaristasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
